b7a8a2c3783c98ac4166c9c044e1d4def41b6765
wyvr <wyvr@daviot.at>
2021-10-05 17:37:49 +0200

    :art: refactor plugin

--- plugins/wyvr/remixicon/index.js
+++ plugins/wyvr/remixicon/index.js
@@ -5,9 +5,14 @@ const { Logger } = require('@lib/logger');
 // by searching unused icons and removing them
 const used_remixicons = [];
 
+const pkg_name = '[package-remixicon]';
+
 module.exports = {
     inject: {
         after: async (err, config, file, content, head, body) => {
+            if(config.env == 'dev') {
+                return [err, config, file, content, head, body];
+            }
             // search for all used icons
             const matches = content.match(/["\s]ri-[^\s"]+/g);
             if (matches) {
@@ -22,14 +27,19 @@ module.exports = {
             return [err, config, file, content, head, body];
         },
     },
-    optimize: {
+    link: {
         before: async (...args) => {
+            const config = args[1];
+            if(config.env == 'dev') {
+                Logger.improve(`${pkg_name} will not be executed in dev mode`);
+                return args;
+            }
             // write used icons file
-            Logger.debug(`[package-remixicon] use ${used_remixicons.length} icons`);
+            Logger.debug(`${pkg_name} use ${used_remixicons.length} icons`);
             const used_regex = new RegExp(`${used_remixicons.join('|')}`);
             const remixicon_file = 'gen/assets/remixicon/remixicon.css';
             if (!fs.existsSync(remixicon_file)) {
-                Logger.error(`[package-remixicon] file does not exist ${remixicon_file}`);
+                Logger.error(`${pkg_name} file does not exist ${remixicon_file}`);
                 return args;
             }
             let removed = 0;
@@ -40,15 +50,19 @@ module.exports = {
                     if (line.trim().indexOf('.ri-') != 0 || (used_remixicons.length > 0 && line.match(used_regex))) {
                         return true;
                     }
-                    removed++;
+                    if (line.trim().indexOf('.ri-') == 0) {
+                        removed++;
+                    }
                     return false;
                 })
                 .join('\n');
-            Logger.debug(`[package-remixicon] remove ${removed} lines from css file`);
+            Logger.debug(`${pkg_name} removed ${removed} lines`);
             if (content) {
+                const percent = 100 - Math.floor((used_remixicons.length / (used_remixicons.length + removed)) * 100);
+                Logger.improve(`${pkg_name} shrinked by ~${percent}%`);
                 fs.writeFileSync('gen/assets/remixicon/remixicon.used.css', content);
             }
             return args;
         },
-    }
+    },
 };
