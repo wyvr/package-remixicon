01d98b66dbf746fe5a64585d7abe741d0682389a
wyvr <wyvr@daviot.at>
2022-06-15 12:32:03 +0200

    :boom: new version of inject

--- src/component/remixicon/Include.svelte
+++ src/component/remixicon/Include.svelte
@@ -1,6 +1,6 @@
 <script>
     import Stylesheet from '@src/component/Stylesheet.svelte';
-    const use_shrinked = getGlobal('global.env', 'dev') == 'prod';
+    const use_shrinked = _inject('global.env', 'dev') == 'prod';
 </script>
 
 {#if use_shrinked}
