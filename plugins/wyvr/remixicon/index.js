d2dfb25246ef99df2089560f107a3cdfc81a203c
wyvr <info@wyvr.dev>
2024-03-09 00:04:30 +0100

    refactor: refactor the plugin

--- plugins/wyvr/remixicon/index.js
+++ plugins/wyvr/remixicon/index.js
@@ -1,66 +1,70 @@
-import { existsSync, readFileSync, writeFileSync } from 'fs';
-import { filled_array } from '@lib/utils/validate.js';
-import { read, write } from '@lib/utils/file.js';
-import { Logger } from '@lib/utils/logger.js';
+import { filled_array } from "@wyvr/generator/src/utils/validate.js";
+import { read, write } from "@wyvr/generator/src/utils/file.js";
+import { logger } from "@wyvr/generator/universal.js";
 
 // plugin to reduce the size of the remixicons
 // by searching unused icons and removing them
 const used_remixicons = [];
 let remixicons_file;
 
-const pkg_name = '[package-remixicon]';
+const pkg_name = "[package-remixicon]";
 
 export default {
-    optimize: {
-        before: async ({ err, args: [files, ...params] }) => {
-            if (filled_array(files)) {
-                files
-                    .filter((file) => {
-                        if (file.indexOf('/assets/remixicon/remixicon.css') > -1) {
-                            remixicons_file = file;
-                        }
-                        return file.match(/\.html?$/);
-                    })
-                    .forEach((file) => {
-                        const content = read(file);
-                        const matches = content.match(/["\s]ri-[^\s"]+/g);
-                        if (matches) {
-                            matches
-                                .map((match) => match.replace(/^./, ''))
-                                .forEach((match) => {
-                                    if (used_remixicons.indexOf(match) == -1) {
-                                        used_remixicons.push(match);
-                                    }
-                                });
-                        }
-                    });
-            }
-            if (remixicons_file) {
-                let removed = 0;
-                const used_regex = new RegExp(`${used_remixicons.join('|')}`);
-                const content = read(remixicons_file)
-                    .split('\n')
-                    .filter((line) => {
-                        if (
-                            line.trim().indexOf('.ri-') != 0 ||
-                            (used_remixicons.length > 0 && line.match(used_regex))
-                        ) {
-                            return true;
-                        }
-                        if (line.trim().indexOf('.ri-') == 0) {
-                            removed++;
-                        }
-                        return false;
-                    })
-                    .join('\n');
-                // override the remixicons file
-                write(remixicons_file, content);
-                if (content) {
-                    const percent = 100 - Math.floor((used_remixicons.length / (used_remixicons.length + removed)) * 100);
-                    Logger.improve(`${pkg_name} removed ${removed} icons - shrinked by ~${percent}%`);
-                }
-            }
-            return { err, args: [files, ...params] };
-        },
-    },
+	optimize: {
+		before: async ({ err, args: [files, ...params] }) => {
+			if (filled_array(files)) {
+				for (const file of files) {
+					if (file.indexOf("/assets/remixicon/remixicon.css") > -1) {
+						remixicons_file = file;
+					}
+					if (file.match(/\.html?$/)) {
+						const content = read(file);
+						const matches = content.match(/["\s]ri-[^\s"]+/g);
+						if (matches) {
+							for (const match of matches) {
+								const cleanedMatch = match.replace(/^./, "");
+								if (used_remixicons.indexOf(cleanedMatch) === -1) {
+									used_remixicons.push(cleanedMatch);
+								}
+							}
+						}
+					}
+				}
+			}
+			if (remixicons_file) {
+				let removed = 0;
+				const used_regex = new RegExp(`${used_remixicons.join("|")}`);
+				const content = read(remixicons_file)
+					.split("\n")
+					.filter((line) => {
+						const index = line.trim().indexOf(".ri-");
+						if (
+							index !== 0 ||
+							(used_remixicons.length > 0 && line.match(used_regex))
+						) {
+							return true;
+						}
+						if (index === 0) {
+							removed++;
+						}
+						return false;
+					})
+					.join("\n");
+				// override the remixicons file
+				write(remixicons_file, content);
+				if (content) {
+					const percent =
+						100 -
+						Math.floor(
+							(used_remixicons.length / (used_remixicons.length + removed)) *
+								100,
+						);
+					logger.improve(
+						`${pkg_name} removed ${removed} icons - shrinked by ~${percent}%`,
+					);
+				}
+			}
+			return { err, args: [files, ...params] };
+		},
+	},
 };
